baa766e90f880c909afe83b54f543fa637506cfc
Tomas Merino <tomasmerinomm@gmail.com>
2025-06-02 23:10:25 +0000

    07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let tmp = string.split("");
+    let result = "";
 
+    for (let i = tmp.length - 1; i >= 0; i--) {
+        result += tmp[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
