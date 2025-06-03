5e33c252dd4b09e847e29b5f4d71f95d2fa150c5
Tomas Merino <tomasmerinomm@gmail.com>
2025-06-03 18:59:39 +0000

    09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    let sum = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(finish) ||
+        start < 0 || finish < 0) {
+        return "ERROR";
+    }
+    
+    if (start > finish) {
+        [start, finish] = [finish, start];
+    }
 
+    for (let i = start; i <= finish; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
